a656ddf85c909214115991cda31b78e90c4bdcf5
Geony Han <geonyhan@Geonyui-MacBookPro.local>
2024-06-02 22:04:39 +0900

    listview

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,22 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:toonflix/models/webtoon_model.dart';
+import 'package:toonflix/services/api_service.dart';
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+  HomeScreen({super.key});
+  final Future<List<WebToonModel>> webtoons = ApiService.getTodaysToons();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          "Today's Toons",
-          style: TextStyle(fontSize: 24, fontWeight: FontWeight.w600),
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          title: const Text(
+            "Today's Toons",
+            style: TextStyle(fontSize: 24, fontWeight: FontWeight.w600),
+          ),
+          foregroundColor: Colors.green,
+          surfaceTintColor: Colors.white,
+          shadowColor: Colors.black,
+          elevation: 2,
         ),
-        foregroundColor: Colors.green,
-        surfaceTintColor: Colors.white,
-        shadowColor: Colors.black,
-        elevation: 2,
-      ),
-      backgroundColor: Colors.white,
-    );
+        body: FutureBuilder(
+          future: webtoons,
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return ListView.separated(
+                scrollDirection: Axis.horizontal,
+                itemCount: snapshot.data!.length,
+                itemBuilder: (context, index) {
+                  var webtoon = snapshot.data![index];
+                  return Text(webtoon.title);
+                },
+                separatorBuilder: (context, index) => const SizedBox(
+                  width: 20,
+                ),
+              );
+            }
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          },
+        ));
   }
 }
--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,19 +1,21 @@
 import 'dart:convert';
-
 import 'package:http/http.dart' as http;
 import 'package:toonflix/models/webtoon_model.dart';
 
 class ApiService {
-  final String baseUrl = "https://webtoon-crawler.nomadcoders.workers.dev";
-  final String today = 'today';
-  List<WebToonModel> webtoonInstances = [];
-  Future<List<WebToonModel>> getTodaysToons() async {
+  static const String baseUrl =
+      "https://webtoon-crawler.nomadcoders.workers.dev";
+  static const String today = 'today';
+
+  static Future<List<WebToonModel>> getTodaysToons() async {
+    List<WebToonModel> webtoonInstances = [];
     final url = Uri.parse('$baseUrl/$today');
     final response = await http.get(url);
     if (response.statusCode == 200) {
       final List<dynamic> webtoons = jsonDecode(response.body);
       for (var webtoon in webtoons) {
-        webtoonInstances.add(WebToonModel.fromJson(webtoon));
+        final instance = WebToonModel.fromJson(webtoon);
+        webtoonInstances.add(instance);
       }
       return webtoonInstances;
     }
